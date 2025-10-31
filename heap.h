f526ae8d91457e236c97a18ba6087eae5834b0e3
BrunoYHayashi <byh25@inf.ufpr.br>
2025-10-31 17:52:58 -0300

    [MOD] modificação no comentário

--- heap.h
+++ heap.h
@@ -5,9 +5,10 @@ struct paciente;
 
 /*
 Tenta alocar a heap
-Em caso de falha, retorna NULL
-*/
+Em caso de falha, retorna NULL 
 em caso de sucesso, retorna o ponteiro para ponteiro da heap
+*/
+
 struct paciente **InicHeap(int N);
 
 /*essa funcao eh o insere heap para o heapfy*/
