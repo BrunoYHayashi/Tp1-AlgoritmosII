7ced1b3f2e39d986e004b4d72aa5e27f83cb458f
Luiz Mo <luizming07@gmail.com>
2025-10-31 17:50:34 -0300

    [mod] adição de altera heap e insereNovoHeap

--- heap.h
+++ heap.h
@@ -6,19 +6,21 @@ struct paciente;
 /*
 Tenta alocar a heap
 Em caso de falha, retorna NULL
-em caso de sucesso, retorna o ponteiro para ponteiro da heap
 */
-struct paciente **InicHeap(int tam);
+em caso de sucesso, retorna o ponteiro para ponteiro da heap
+struct paciente **InicHeap(int N);
 
-/*insere um elemento na heap e retorna 1 em caso de sucesso e 0 em caso de falha*/
-int InsereHeap(struct paciente *heap[], struct paciente *p, int tam);
+/*essa funcao eh o insere heap para o heapfy*/
+void InsereHeap(struct paciente *heap[], int tam);
+
+/*insere novo elemento na heap e retorna 1 em caso de sucesso e 0 caso contrario*/
+int InsereNovoHeap(struct paciente *heap[], struct paciente *p, int tam);
 
 /*
 remove um elemento da heap
 retorna 1 em caso de sucesso e 0 caso o elemento nao exista na heap
 */
-int RemoveHeap(struct paciente *heap[], struct paciente *p,int tam);
-
+void RemoveHeap(struct paciente *heap[], int *tam);
 
 /*transforma o vetor em uma heap*/
 void Heapfy(struct paciente *heap[], int tam);
@@ -31,5 +33,8 @@ void ImprimeHeap(struct paciente *heap[], int tam);
 
 /*ordena a heap utilizando o Heap Sort*/
 void HeapSort(struct paciente *heap[], int tam);
+
+/*altera a prioridade do paciente indicado pelo parametro nome[]*/
+void AlteraHeap(struct paciente *heap[], char nome[], int prioridade, int tam);
 
 #endif
