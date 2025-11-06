2dcb2a3166555bb359a88940b79d731ce4b8c9fc
Luiz Mo <luizming07@gmail.com>
2025-11-06 20:30:37 -0300

    [mod]finalizado o log.c

--- log.c
+++ log.c
@@ -45,8 +45,15 @@ void HeapRemove(struct paciente *heap[], int *tam){
     }
 
     printf("Paciente %s de prioridade %d foi removido do heap",p->nome,p->prioridade);
-    free(p->nome);
     free(p);
+}
+
+void HeapCheca(struct paciente *heap[], int tam){
+    if(!ChecaHeap(heap,tam)){
+        printf("ChecaHeap: Nao eh uma heap");
+        return;
+    }
+    printf("ChecaHeap: Eh uma heap");
 }
 
 int main(){
@@ -57,11 +64,12 @@ int main(){
     printf("Inicio da execucao do programa de teste\n");
     printf("===================================================================\n");
 
+    printf("Tentando iniciar a heap, que pode ter no maximo %d elementos\n", MAX);
     heap = InicHeap(MAX,&tam);
     if(tam == 0)
-        printf("Heap vazia\n");
+        printf("Heap vazia\n\n");
 
-    printf("Insere 5 pacientes na heap\n");
+    printf("Insere 5 pacientes na heap\n\n");
     strcpy(nome, "Marchiorato");
     prio = 1;
     HeapInsere(heap,nome,prio,&tam,MAX);
@@ -97,7 +105,7 @@ int main(){
     ImprimeHeap(heap,tam);
     printf("\n");
 
-    printf("Tenta inserir mais um paciente");
+    printf("Tenta inserir mais um paciente\n");
     strcpy(nome, "Juliano");
     prio = 1;
     HeapInsere(heap,nome,prio,&tam,MAX);
@@ -105,6 +113,11 @@ int main(){
     ImprimeHeap(heap,tam);
     printf("\n");
 
+    printf("Verificando se eh uma heap\n");
+    HeapCheca(heap,tam);
+    printf("\n");
+
+    printf("Alterando a prioridade de dois pacientes\n\n");
     strcpy(nome, "Marchiorato");
     prio = 3;
     HeapAtualiza(heap,nome,prio,tam);
@@ -130,14 +143,24 @@ int main(){
     printf("Heapfy fez %d comparacoes e %d trocas", n_comp,n_trocas);
     printf("\nVeja a HEAP:\n");
     ImprimeHeap(heap,tam);
+    printf("\n");
+
+    printf("Verificando se eh uma heap\n");
+    HeapCheca(heap,tam);
     printf("\n\n");
 
-    printf("Remove os 5 pacientes e tenta remover mais um\n");
+    printf("Remove os 5 pacientes e tenta remover mais um\n\n");
     for(i=1;i<=6;i++){
         HeapRemove(heap,&tam);
         printf("\nHEAP:\n");
         ImprimeHeap(heap,tam);
         printf("\n");
     }
+
+    if(tam == 0)
+        printf("Heap vazia\n");
+
+    free(heap);
+    printf("\nFim do programa de teste\n");
 }
 
